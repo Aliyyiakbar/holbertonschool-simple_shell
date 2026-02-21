bba48427e2a9b70a1a9bc2353e2d8c319bf52b38
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-02-22 01:43:55 +0400

    Refactor b_cd and cd_dst functions: improve target resolution logic and enhance readability

--- cd.c
+++ cd.c
@@ -1,6 +1,38 @@
 #include "bash.h"
 
 /**
+ * cd_dst - get cd target
+ * @av: argv array
+ * @dst: target out
+ * @st: status pointer
+ * Return: 1 to continue, 0 to stop
+ */
+static int cd_dst(char **av, char **dst, int *st)
+{
+	*dst = av[1];
+	if (*dst == NULL)
+	{
+		*dst = env_get("HOME");
+		if (*dst == NULL || (*dst)[0] == '\0')
+		{
+			*st = 0;
+			return (0);
+		}
+	}
+	if (s_cmp(*dst, "-") == 0)
+	{
+		*dst = env_get("OLDPWD");
+		if (*dst == NULL)
+		{
+			fprintf(stderr, "cd: OLDPWD not set\n");
+			*st = 1;
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
  * b_cd - change directory
  * @av: argv array
  * @st: status pointer
@@ -10,24 +42,12 @@ int b_cd(char **av, int *st)
 {
 	char *dst, *old, *nw;
 
-	dst = av[1] ? av[1] : env_get("HOME");
-	if (dst == NULL)
+	if (!cd_dst(av, &dst, st))
 	{
-		*st = 0;
 		return (BUILTIN_HANDLED);
 	}
-	if (dst && s_cmp(dst, "-") == 0)
-	{
-		dst = env_get("OLDPWD");
-		if (dst == NULL)
-		{
-			fprintf(stderr, "cd: OLDPWD not set\n");
-			*st = 1;
-			return (BUILTIN_HANDLED);
-		}
-	}
 	old = getcwd(NULL, 0);
-	if (dst == NULL || chdir(dst) != 0)
+	if (chdir(dst) != 0)
 	{
 		perror("cd");
 		free(old);
